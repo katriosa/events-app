b730e1ffa5c3d479d8755487e3427fdbc4c816d2
katriosa <ee.chistyakova@gmail.com>
2023-03-01 22:29:16 +0700

    action for EventForm

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import EventRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
