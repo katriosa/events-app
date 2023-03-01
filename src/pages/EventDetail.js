e562c6940189b99646ffc749a47d5c055023f540
katriosa <ee.chistyakova@gmail.com>
2023-03-01 15:17:31 +0700

    dynamic routes loader

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,12 +1,23 @@
-import { useParams } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-  const params = useParams();
-  return (
-    <>
-      <h1>Event Detail Page</h1>
-      <p>Event ID: {params.id}</p>
-    </>
-  );
+  const data = useLoaderData();
+
+  return <EventItem event={data.event} />;
 };
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.id;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event" },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
+}
