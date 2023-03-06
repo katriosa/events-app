d8641d5fdc5f28bba63c1d1eff4e8d3b9124a31a
katriosa <ee.chistyakova@gmail.com>
2023-03-06 13:44:37 +0700

    signup

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ import EventRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -54,7 +56,11 @@ const router = createBrowserRouter([
           },
         ],
       },
-      { path: "auth", element: <AuthenticationPage /> },
+      {
+        path: "auth",
+        element: <AuthenticationPage />,
+        action: authAction,
+      },
       {
         path: "newsletter",
         element: <NewsletterPage />,
