ccb480ffe17173baa7e1ce131a1fbb59c2c7133a
katriosa <ee.chistyakova@gmail.com>
2023-03-06 10:51:29 +0700

    update files for AUTH SECTION

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,19 +1,21 @@
-import PageContent from "../components/PageContent";
-import { useRouteError } from "react-router-dom";
-import MainNavigation from "../components/MainNavigation";
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
 
-const ErrorPage = () => {
+import PageContent from '../components/PageContent';
+
+function ErrorPage() {
   const error = useRouteError();
 
-  let title = "An error occurred!";
-  let message = "Something went wrong";
+  let title = 'An error occurred!';
+  let message = 'Something went wrong!';
 
   if (error.status === 500) {
     message = error.data.message;
   }
+
   if (error.status === 404) {
-    title = "Not found!";
-    message = "Could not find resource or page";
+    title = 'Not found!';
+    message = 'Could not find resource or page.';
   }
 
   return (
@@ -24,5 +26,6 @@ const ErrorPage = () => {
       </PageContent>
     </>
   );
-};
+}
+
 export default ErrorPage;
